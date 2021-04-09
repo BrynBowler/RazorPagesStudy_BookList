fa5e787052f8755b9db25cca86c08906a9b0cab4
Bryn Bowler <b.bowler@mail.com>
2021-04-09 14:26:01 +0100

    Book Class creation in preperation for establishing Database Connection

---
+++ Model/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_Study_BookList.Model
+{
+    public class Book
+    {
+        [System.ComponentModel.DataAnnotations.Key]
+        public int Id { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required]
+        public string Name { get; set; }
+
+        public string Author { get; set; }
+    }
+}
