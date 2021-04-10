fb7cfb259c3b0da3a3ec98dfc11b5853ad5c0dab
Bryn Bowler <b.bowler@mail.com>
2021-04-10 16:16:15 +0100

    Began Local Database Connetion Process

---
+++ Model/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_Study_BookList.Model
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(Microsoft.EntityFrameworkCore.DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Book> Book { get; set; }
+    }
+}
